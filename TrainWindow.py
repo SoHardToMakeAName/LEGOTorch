dc4829075167cb4821c96db6abeefc54902f384d
SoHardToMakeAName <1747357541@qq.com>
2020-04-09 21:57:51 +0800

    最近进度变慢，要加油！

--- TrainWindow.py
+++ TrainWindow.py
@@ -10,14 +10,33 @@ class TrainWindow(QtWidgets.QWidget, Ui_UI_TrainWindow):
     def __init__(self):
         super(TrainWindow, self).__init__()
         self.setupUi(self)
-        with open("tmp.py", 'w') as self.f:
-            self.f.write("import torch\nimport torch.nn as nn\n")
         self.model = dict()
+        self.dataset = dict()
+        self.optim = dict()
+        self.loss_function = dict()
 
     def load_model(self):
-        print("load_model run")
         if self.model_to_load.currentIndex() == 1:
             filename, _ = QFileDialog.getOpenFileName(self, '加载模型', 'C:\\', 'Python Files (*.py)')
             filedir, filename_text = os.path.split(filename)
-            self.f.write("From {} import Net\n".format(filename_text.split(".")[0]))
-            self.log.append("从{}加载模型\n".format(filename))
+            self.model['type'] = 1
+            self.model['dir'] = filedir
+            self.model['module'] = filename_text.split(".")[0]
+            for k, v in self.model.items():
+                print(k, v)
+        else:
+            self.is_pretrained = QCheckBox("使用预训练模型")
+            self.pretrained_layout.addWidget(self.is_pretrained)
+            self.model['type'] = 2
+            self.model['name'] = self.model_to_load.currentText()
+            for k, v in self.model.items():
+                print(k, v)
+
+    def load_dataset(self):
+        pass
+
+    def load_loss_function(self):
+        pass
+
+    def load_optim(self):
+        pass
